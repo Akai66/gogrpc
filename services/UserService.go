e5c32548de243d7af67d6daca107d5bc014c5b4d
liukai <869896679@qq.com>
2021-09-22 15:03:08 +0800

    服务端流模式

--- services/UserService.go
+++ services/UserService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"time"
 )
 
 // UserService 实现UserServiceServer接口
@@ -19,4 +20,32 @@ func (this *UserService) GetUserScore(ctx context.Context, userReq *UserRequest)
 		users = append(users,user)
 	}
 	return &UserResponse{Users: users},nil
+}
+
+func (this *UserService) GetUserScoreByStream(userReq *UserRequest, stream UserService_GetUserScoreByStreamServer) error {
+	var score int32 = 101
+	users := make([]*UserInfo,0)
+	for index,user := range userReq.Users {
+		user.UserScore = score
+		score++
+		users = append(users,user)
+		if (index + 1) % 2 == 0 {
+			//每获取2个用户积分，就返回一次
+			err := stream.Send(&UserResponse{Users: users})
+			if err != nil {
+				return err
+			}
+			//清空users
+			users = users[0:0]
+		}
+		time.Sleep(1* time.Second)
+	}
+	//用户数量为奇数时，处理users中多出的部分
+	if len(users) > 0 {
+		err := stream.Send(&UserResponse{Users: users})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
